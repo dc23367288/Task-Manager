fb8cff9067deadf24f8863ae3440ac91e6ba0d13
dc23367288 <dean.coles68@gmail.com>
2025-07-05 15:26:27 +0100

    Delete firebase.js

--- firebase.js
+++
@@ -1,6 +0,0 @@
-const firebaseConfig = { 
-  apiKey: "YOUR_KEY", authDomain: "...", projectId: "..."
-};
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
